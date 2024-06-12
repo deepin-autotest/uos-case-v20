3f7d4ffb17dd8686127a2110af90ff4573a20e65
mikigo <huangmingqiang@uniontech.com>
2024-06-12 10:28:51 +0800

    fix:
    Description:
    
    Log:

--- case/test_dfm_001.py
+++ case/test_dfm_001.py
@@ -5,6 +5,6 @@ class TestDfm:
 
     def test_dfm_001(self):
         """任务栏启动文管"""
-        location = Dogtail().element_center("Btn_文件管理器")
+        location = Dogtail().ele("Btn_文件管理器").click()
         print(location)
         assert location
