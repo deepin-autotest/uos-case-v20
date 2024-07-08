698a06b6927c052af6351efcac2d5e7b10b56ecd
mikigo <huangmingqiang@uniontech.com>
2024-07-08 17:05:20 +0800

    update

--- case/dde_file_manager/test_dfm_1159151.py
+++ case/dde_file_manager/test_dfm_1159151.py
@@ -9,7 +9,7 @@ class TestDfm(BaseCase):
     def test_dfm_1159151(self):
         """任务栏启动文管"""
         DdeDockMethod().right_click_dde_file_manager_on_dock_by_attr()
-        DdeDockMethod.select_menu(1)
+        DdeDockMethod().click_open_in_right_menu_by_mk()
         sleep(2)
         self.assert_process_exist("dde-file-manager")
 
--- method/dde_dock_method.py
+++ method/dde_dock_method.py
@@ -1,29 +1,29 @@
-from funnylog.conf import setting as log_setting
+from funnylog2.config import config as log_config
 from youqu3 import log
-from youqu3.dogtail import Dogtail
-from youqu3.mousekey import MouseKey
+from youqu3.gui import pylinuxauto
 from youqu3.cmd import Cmd
 
-log_setting.CLASS_NAME_ENDSWITH = "Method"
+log_config.CLASS_NAME_ENDSWITH = "Method"
 
 
 @log
-class DdeDockMethod(MouseKey, Cmd):
+class DdeDockMethod(Cmd):
 
-    @property
-    def dock(self):
-        return Dogtail(appname="dde-dock")
 
     def click_dde_file_manager_on_dock_by_attr(self):
         """在任务栏点击文件管理器"""
-        self.dock.ele("Btn_文件管理器").click()
+        pylinuxauto.find_element_by_attr_path("/dde-dock/Btn_文件管理器").click()
 
     def right_click_dde_file_manager_on_dock_by_attr(self):
         """在任务栏右键点击文件管理器"""
-        self.dock.ele("Btn_文件管理器").right_click()
+        pylinuxauto.find_element_by_attr_path("/dde-dock/Btn_文件管理器").right_click()
 
-
+    def click_open_in_right_menu_by_mk(self):
+        """点击右键菜单中【打开】"""
+        pylinuxauto.select_menu(1)
 
 
 if __name__ == '__main__':
-    DdeDockMethod().click_dde_file_manager_on_dock_by_attr()
+    DdeDockMethod().right_click_dde_file_manager_on_dock_by_attr()
+    DdeDockMethod().click_open_in_right_menu_by_mk()
+
